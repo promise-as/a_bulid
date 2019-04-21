87f731a9f91a31c9bdccf7f5a8886787c8b4ed99
Hong Baojin <13809735212@163.com>
2019-04-22 00:54:06 +0800

    write to right-nav

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,13 +1,19 @@
 'use strict';
 
 $(function () {
-    $('.check-item input:radio').click(function (e) {
-        // console.log(1111, e);
-        e.stopPropagation();
-        // 如果想要找兄弟元素，先找其父元素然后再找儿子
-        // $(this).parents('.check-item').find('.user-con').removeClass('error');
-        $(this).parents('.check-item').find('.user-education').val($(this).val());
-        $(this).parents('.check-item').find('.user-job').val($(this).val());
-        $(this).parents('.check-item').find('.user-major').val($(this).val());
-    });
+  // 在线审核
+  $('.check-item input:radio').click(function (e) {
+    // console.log(1111, e);
+    e.stopPropagation();
+    // 如果想要找兄弟元素，先找其父元素然后再找儿子
+    // $(this).parents('.check-item').find('.user-con').removeClass('error');
+    $(this).parents('.check-item').find('.user-education').val($(this).val());
+    $(this).parents('.check-item').find('.user-job').val($(this).val());
+    $(this).parents('.check-item').find('.user-major').val($(this).val());
+  });
+
+  // 关闭广告
+  $(".close").click(function () {
+    $(".ad").css("display", "none");
+  });
 });
